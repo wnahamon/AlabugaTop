6f58f0f06182a1ac277258be3e52e2f4a071007b
StudentBI28 <StudentBI28@SYN13ASS-NB-503.alabuga.local>
2024-12-28 15:25:30 +0300

    CRUD

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse_lazy
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    product = models.ManyToManyField(Product, verbose_name='Product')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy('main')
+
+class FAQ(models.Model):
+    question = models.TextField(max_length=500)
+    answer = models.CharField(max_length=250)
